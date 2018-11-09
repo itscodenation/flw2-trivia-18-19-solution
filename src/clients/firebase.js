28646bc373a8ba9e1f5c9fc1a6508007940fbed0
Peter <pwjablonski@gmail.com>
2018-11-09 15:36:34 -0500

    add gh-pages

--- src/clients/firebase.js
+++ src/clients/firebase.js
@@ -1,6 +1,9 @@
 import * as firebase from 'firebase';
+import once from 'lodash/once';
 
-async function loadDatabase() {
+const loadDatabase = buildFirebase();
+
+function buildFirebase() {
     const app = firebase.initializeApp({
       apiKey: "AIzaSyCpc1noPpADSu0vm5AYdUmNSnjEUMt5ZQI",
       authDomain: "studio2-331cf.firebaseapp.com",
@@ -8,13 +11,23 @@ async function loadDatabase() {
       projectId: "studio2-331cf",
       storageBucket: "studio2-331cf.appspot.com",
       messagingSenderId: "1016551157105"
-    }, "Studio2");
-  
-    return firebase.database(app);
+    });
+
+    return once(async() => {
+      return firebase.database(app);
+    })
 }
 
 export async function getQuestions() {
   const database = await loadDatabase();
-  const databaseValues = await database.ref('/').once('value');
-  return databaseValues.val();
+  const questions = await database.ref('/questions').once('value');
+  return questions.val();
+}
+
+export async function getRandomQuestion() {
+  const questions = await getQuestions();
+  const randomIndex = Math.floor(Math.random() * questions.length);
+  console.log(questions)
+  console.log(randomIndex)
+  return questions[randomIndex];
 }
