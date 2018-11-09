d7bd578f95d148708d9ec9f4c06d33621a71b802
Peter <pwjablonski@gmail.com>
2018-11-09 15:33:41 -0500

    Updates

---
+++ precache-manifest.4cca74d0ce85677e33ea9e24853b7fbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a8a3bf4dde7e013c37d",
+    "url": "/studio2/static/js/runtime~main.3a8a3bf4.js"
+  },
+  {
+    "revision": "eb339fb05888efe41a96",
+    "url": "/studio2/static/js/main.eb339fb0.chunk.js"
+  },
+  {
+    "revision": "00646c0fc1df48d69a20",
+    "url": "/studio2/static/js/1.00646c0f.chunk.js"
+  },
+  {
+    "revision": "eb339fb05888efe41a96",
+    "url": "/studio2/static/css/main.68c011d6.chunk.css"
+  },
+  {
+    "revision": "78daf7830669fcb7486735d88daeaf3b",
+    "url": "/studio2/index.html"
+  }
+];
