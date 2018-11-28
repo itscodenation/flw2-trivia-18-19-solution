d3ffb5071fd54fe4cfe12a740d3f029cad02d884
Peter <pwjablonski@gmail.com>
2018-11-28 13:18:05 -0500

    starter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,90 +1,17 @@
 import React, { Component } from 'react';
 import '../css/App.css';
-
-import QuestionDisplay from './QuestionDisplay';
-import {
-  getRandomQuestion,
-} from '../clients/firebase';
+// import the question display component
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentQuestion: null,
-      shouldShowCorrectAnswer: false,
-      shouldShowResetButton: false,
-      seconds: 10,
-      correctGuesses: 0,
-    };
-  }
+  // add your constructor
 
-  componentDidMount(){
-    this.setRandomQuestion();
-    this.startTimer();
-  }
-
-  _onAnswerButtonClicked(isCorrect){
-    if(isCorrect){
-      this.setState({correctGuesses: this.state.correctGuesses + 1})
-    }
-    console.log("isCorrect");
-    this.showCorrectAnswer()
-    this.showResetButton()
-  }
-
-  _onResetButtonClicked(){
-    this.hideResetButton();
-    this.hideCorrectAnswer();
-    this.setRandomQuestion();
-    this.startTimer();
-  }
-
-  startTimer(){
-    this.setState({seconds: 10})
-    let timer = setInterval(() => {
-      if(this.state.seconds === 0) {
-        clearInterval(timer);
-        this.showCorrectAnswer();
-        this.showResetButton();
-      } else {
-        this.setState({seconds: this.state.seconds - 1})
-      }
-    }, 1000);
-  }
-
-  showCorrectAnswer(){
-    this.setState({shouldShowCorrectAnswer: true})
-  }
-
-  hideCorrectAnswer(){
-    this.setState({shouldShowCorrectAnswer: false})
-  }
-
-  showResetButton(){
-    this.setState({shouldShowResetButton: true})
-  }
-
-  hideResetButton(){
-    this.setState({shouldShowResetButton: false})
-  }
-
-  async setRandomQuestion(){
-    const randomQuestion = await getRandomQuestion();
-    this.setState({currentQuestion: randomQuestion});
-  }
 
   render() {
     return (
       <div className="app">
-        <QuestionDisplay
-          seconds={this.state.seconds}
-          correctGuesses={this.state.correctGuesses}
-          shouldShowCorrectAnswer={this.state.shouldShowCorrectAnswer}
-          shouldShowResetButton={this.state.shouldShowResetButton}
-          question={this.state.currentQuestion}
-          onAnswerButtonClicked={(test)=>this._onAnswerButtonClicked(test)}
-          onResetButtonClicked={()=>this._onResetButtonClicked()}
-        />
+        
+        {/* Add your question display component */}
+        
       </div>
     );
   }
--- src/components/QuestionDisplay.jsx
+++ src/components/QuestionDisplay.jsx
@@ -1,60 +1,15 @@
 import React from 'react';
-import QuestionStem from './QuestionStem';
-import AnswerButton from './AnswerButton';
-import ResetButton from './ResetButton';
-import Timer from './Timer';
-import Counter from './Counter';
+// import your components here
 
 
-export default function Question({
-  correctGuesses,
-  question,
-  seconds,
-  shouldShowCorrectAnswer,
-  shouldShowResetButton,
-  onResetButtonClicked,
-  onAnswerButtonClicked,
-}) {
-  if (!question){
-    return null;
-  }
+export default function Question() {
   return (
     <div className="questionDisplay">
-      <div className="questionStem">
-        <QuestionStem
-          questionStem={question.questionStem}
-        />
-      </div>
-      <div className="timer">
-        <Timer
-          seconds={seconds}
-        />
-      </div>
-      <div className="counter">
-        <Counter
-          correctGuesses={correctGuesses}
-        />
-      </div>
-      <div className="reset_button">
-        <ResetButton 
-          shouldShowResetButton={shouldShowResetButton}
-          onResetButtonClicked={onResetButtonClicked}
-        />
-      </div>
-      <div className="buttons">
-        {
-          question.answerChoices.map(answerChoice => {
-            return (
-              <AnswerButton
-                answerChoice={answerChoice}
-                shouldShowCorrectAnswer={shouldShowCorrectAnswer}
-                onAnswerButtonClicked={onAnswerButtonClicked}
-                key={answerChoice.answer}
-              />
-            )
-          })
-        }
-      </div>
+      {/* Add QuestionStem */}
+      {/* Add Timer  */}
+      {/* Add Counter */}
+      {/* Add Reset Button */}
+      {/* Add Buttons */}
     </div>
   );
 }
