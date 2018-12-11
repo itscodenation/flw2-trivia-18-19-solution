7972cf40b51917a05d07966843c366abafc27eca
Peter <pwjablonski@gmail.com>
2018-12-11 17:47:16 -0500

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,43 +5,18 @@ import {getRandomQuestion} from '../clients/firebase'
 import Question from './Question'
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state= {
-      questionText: null,
-      answerChoiceOne: null,
-      answerChoiceTwo: null,
-      answerChoiceThree: null,
-      answerChoiceFour: null,
-    }
-  }
 
-  componentWillMount(){
-    this.setCurrentQuestion();
-  }
+  render() { 
 
-  async setCurrentQuestion(){ 
-    var randomQuestion = await getRandomQuestion();
-    this.setState({
-      questionText: randomQuestion.question_text,
-      answerChoiceOne: randomQuestion.choices[0],
-      answerChoiceTwo: randomQuestion.choices[1],
-      answerChoiceThree: randomQuestion.choices[2],
-      answerChoiceFour: randomQuestion.choices[3],
-    })
-  }
-
-  render() {
     return (
       <div className="app">
         Kahoot!
         <Question 
-          questionText={this.state.questionText}
-          answerChoiceOne={this.state.answerChoiceOne}
-          answerChoiceTwo={this.state.answerChoiceTwo}
-          answerChoiceThree={this.state.answerChoiceThree}
-          answerChoiceFour={this.state.answerChoiceFour}
-          resetButtonClicked={()=>this.onResetButtonClicked()}
+          questionText={"Question"}
+          answerChoiceOne={"Answer 1"}
+          answerChoiceTwo={"Answer 2"}
+          answerChoiceThree={"Answer 3"}
+          answerChoiceFour={"Answer 4"}
         />
       </div>
     );
